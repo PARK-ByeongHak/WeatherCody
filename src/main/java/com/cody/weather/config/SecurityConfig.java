02bb31441b9410c41b8fd2f23930f270e3f569fe
PARK-ByeongHak <pbh9696@gmail.com>
2023-12-13 21:43:15 +0900

    SecurityConfig.java 파일 수정

--- src/main/java/com/cody/weather/config/SecurityConfig.java
+++ src/main/java/com/cody/weather/config/SecurityConfig.java
@@ -6,9 +6,9 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 
@@ -23,18 +23,20 @@ public class SecurityConfig {
                     .requestMatchers("/signup").permitAll()
                     .requestMatchers("/user/profile").authenticated()
             )
-            .formLogin(withDefaults())
-            .loginPage("/login")
-            .permitAll()
-            .defaultSuccessUrl("/")
-            .failureUrl("/login?error=true")
-            .and()
-            .logout()
-            .logoutUrl("/logout")
-            .permitAll()
-            .and()
-            .csrf().disable()
-            .headers().frameOptions().disable();
+            .formLogin(loginConfigurer ->
+            loginConfigurer
+                .loginPage("/login")
+                .permitAll()
+                .defaultSuccessUrl("/")
+                .failureUrl("/login?error=true")
+        )
+        .logout(logoutConfigurer ->
+            logoutConfigurer
+                .logoutUrl("/logout")
+                .permitAll()
+        )
+        .csrf().disable()
+        .headers().frameOptions().disable();
         return http.build();
     }
 
